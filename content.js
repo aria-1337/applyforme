f6ad0d9a6984dd691991d47b64afad882338de6b
aria-1337 <aria.lopez.dev@gmail.com>
2024-07-04 20:25:57 -0700

    Get primary LI in an array

--- content.js
+++ content.js
@@ -1,18 +1,29 @@
 const sleep = ms => new Promise(res => setTimeout(res, ms));
 
-async function getJobContainer() {
+// @getJobContainer()
+// Entry point - gets the UL containing al LI with each individual job item on the left hand scroll bar.
+// returns-> all the children in the container.
+async function getJobs() {
     const jobContainer = document.getElementsByClassName('scaffold-layout__list-container')?.[0];
     await sleep(100);
     if (!jobContainer) {
-        return await getJobContainer();
+        return await getJobs();
     }
-    return jobContainer;
+    const jobs = [];
+    const children = jobContainer.childNodes;
+    for (child of children) {
+        if (child.nodeName == 'LI') {
+            jobs.push(child);
+        }
+    }
+    return jobs;
 }
+
 
 (async () => {
     while(true) {
-        const jobContainer = await getJobContainer();
-        await sleep(1000);
-        console.log(jobContainer);
+        const jobs = await getJobs();
+        console.log(jobs);
+        break;
     }
 })();
